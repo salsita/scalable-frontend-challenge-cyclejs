d85d9205fde14113d35aac2c0949a0a9014bdc41
Ondřej Kupka <ondra.cap@gmail.com>
2016-03-17 18:21:37 +0100

    wip [ci skip]

---
+++ src/RemovableGifViewer.js
@@ -0,0 +1,27 @@
+import isolate from '@cycle/isolate';
+import { div, button } from '@cycle/dom';
+
+import Uuid from 'uuid';
+
+import GifViewer from './GifViewer';
+
+
+export default function RemovableGifViewer(sources) {
+  const child = isolate(GifViewer, Uuid.v4())(sources);
+  
+  const remove$ = sources.DOM.select('.btn-remove').events('click');
+
+  const vtree$ = child.DOM
+    .map(childVTree =>
+      div([
+        childVTree,
+        button('.btn-remove', 'Remove')
+      ])
+    );
+
+  return {
+    ...child,
+    remove$,
+    DOM: vtree$
+  };
+}
