6e6196ae75e815d3e1d3a9b38f7d600198e71b8d
Ondřej Kupka <ondra.cap@gmail.com>
2016-03-17 17:02:13 +0100

    wip [ci skip]

--- src/GifViewer.js
+++ src/GifViewer.js
@@ -3,21 +3,11 @@ import { div, h2, img, button } from '@cycle/dom';
 
 export default function GifViewer(sources) {
 
-  const topic = sources.topic;
-
   /*
-   * HTTP - incoming
+   * Sources
    */
 
-  const src$ = sources.HTTP
-    // Turn a stream of streams of responses into a stream of responses.
-    .mergeAll()
-    // Drop responses that are not OK or the image URL is missing.
-    .filter(res => res.ok && res.body.data.image_url)
-    // Map responses to the associated image URLs.
-    .map(res => res.body.data.image_url)
-    // Start with the loading animation initially.
-    .do(src => console.log(`src$ (${topic}): ${src}`));
+  const { src$, topic } = sources;
 
   /*
    * Intent
@@ -30,8 +20,7 @@ export default function GifViewer(sources) {
    */
 
   const state$ = src$
-    .map(src => ({src}))
-    .startWith({src: './loading.gif'});
+    .map(src => ({src, topic}));
 
   /*
    * View
@@ -40,34 +29,18 @@ export default function GifViewer(sources) {
   const vtree$ = state$
     .map(state => 
       div([
-        h2(topic),
+        h2(state.topic),
         img('.gif-view', {src: state.src}),
         button('.more-please', 'More please!')
       ])
-    )
-    .do(req => console.log(`vtree$ (${topic}):`, req))
-
-  /*
-   * HTTP - outgoing
-   */
-
-  const requests$ = morePlease$
-    // Send max 1 request per second.
-    .throttle(1000)
-    // We need to dispatch initial request on startup.
-    .startWith(null)
-    .map(() => ({
-      url: 'http://api.giphy.com/v1/gifs/random',
-      query: {
-        api_key: 'dc6zaTOxFJmzC',
-        tag: topic
-      }
-    }))
-    .do(req => console.log(`requests$ (${topic}):`, req))
+    );
 
   /*
    * Sinks
    */
 
-  return {DOM: vtree$, HTTP: requests$, morePlease$};
+  return {
+    $morePlease,
+    DOM: vtree$
+  };
 }
